92e1921b649c8512f7461249715849fd593d41e4
rizts <rizts.tech@gmail.com>
2025-08-20 15:52:51 +0700

    change umami script position

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,13 +10,13 @@ export default function Home() {
         <title>Risdy – Senior PHP & React Developer</title>
         <meta name="description" content="Portfolio & services" />
         <link rel="icon" href="/assets/images/rizts.png" type="image/png" />
-        <Script
-          async
-          defer
-          data-website-id="32599a5f-7c0f-43c3-992a-44c9b2892e15"
-          src="https://umami-omega-lemon.vercel.app/script.js"
-        />
       </Head>
+      <Script
+        async
+        defer
+        data-website-id="32599a5f-7c0f-43c3-992a-44c9b2892e15"
+        src="https://umami-omega-lemon.vercel.app/script.js"
+      />
       <Hero />
     </Layout>
   )
