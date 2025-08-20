55e2ad9adb017ed41cfb09a1fee6680c04ed3509
rizts <rizts.tech@gmail.com>
2025-08-20 15:44:33 +0700

    add umami script

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Layout from '@/components/Layout'
+import Script from "next/script"
 import Hero from '@/components/Hero'
 
 export default function Home() {
@@ -9,6 +10,12 @@ export default function Home() {
         <title>Risdy – Senior PHP & React Developer</title>
         <meta name="description" content="Portfolio & services" />
         <link rel="icon" href="/assets/images/rizts.png" type="image/png" />
+        <Script
+          async
+          defer
+          data-website-id="32599a5f-7c0f-43c3-992a-44c9b2892e15"
+          src="https://umami-omega-lemon.vercel.app/script.js"
+        />
       </Head>
       <Hero />
     </Layout>
