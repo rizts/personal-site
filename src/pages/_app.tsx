5e9f57a37726c185881c1f2369be98de23d07735
rizts <rizts.tech@gmail.com>
2025-08-20 16:12:57 +0700

    move umami script to MyApp

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,7 @@ import { AnimatePresence, motion } from 'framer-motion'
 import { useRouter } from 'next/router'
 import { useEffect } from 'react'
 import { useThemeStore } from '@/store/themeStore'
+import Script from 'next/script'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter()
@@ -13,17 +14,40 @@ export default function MyApp({ Component, pageProps }: AppProps) {
     initThemeByTime()
   }, [initThemeByTime])
 
+  // Tambahkan hook Umami SPA tracking
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      if ((window as any).umami) {
+        (window as any).umami.track(url)
+      }
+    }
+
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
   return (
-    <AnimatePresence mode="wait">
-      <motion.div
-        key={router.asPath}
-        initial={{ x: '100%' }}
-        animate={{ x: 0 }}
-        exit={{ x: '-100%' }}
-        transition={{ duration: 0.5, ease: 'easeInOut' }}
-      >
-        <Component {...pageProps} />
-      </motion.div>
-    </AnimatePresence>
+    <>
+      {/* Script Umami */}
+      <Script
+        src="https://umami-omega-lemon.vercel.app/script.js"
+        data-website-id="32599a5f-7c0f-43c3-992a-44c9b2892e15"
+        strategy="afterInteractive"
+      />
+
+      <AnimatePresence mode="wait">
+        <motion.div
+          key={router.asPath}
+          initial={{ x: '100%' }}
+          animate={{ x: 0 }}
+          exit={{ x: '-100%' }}
+          transition={{ duration: 0.5, ease: 'easeInOut' }}
+        >
+          <Component {...pageProps} />
+        </motion.div>
+      </AnimatePresence>
+    </>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,5 @@
 import Head from 'next/head'
 import Layout from '@/components/Layout'
-import Script from "next/script"
 import Hero from '@/components/Hero'
 
 export default function Home() {
@@ -11,12 +10,6 @@ export default function Home() {
         <meta name="description" content="Portfolio & services" />
         <link rel="icon" href="/assets/images/rizts.png" type="image/png" />
       </Head>
-      <Script
-        async
-        defer
-        data-website-id="32599a5f-7c0f-43c3-992a-44c9b2892e15"
-        src="https://umami-omega-lemon.vercel.app/script.js"
-      />
       <Hero />
     </Layout>
   )
